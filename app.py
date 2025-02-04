cf46ea94b8cb0c217ec5758e6e0e20a0e7a8917f
siyeol97 <leesi2830@naver.com>
2025-02-04 16:31:22 +0900

    app.py: ffmpeg 경로를 하드코딩으로 설정하고 주석 처리된 코드 제거

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 import os
 import io
 from pydub import AudioSegment
-from shutil import which
+#from shutil import which
 
 
 app = Flask(__name__)
@@ -15,7 +15,8 @@ app = Flask(__name__)
 depress_model = init_depress_model()
 
 # ffmpeg 경로 설정
-AudioSegment.converter = which("ffmpeg")
+#AudioSegment.converter = which("ffmpeg")
+AudioSegment.converter = "/opt/bin/ffmpeg"
 
 # URL로부터 오디오 파일을 가져오는 함수
 def get_audio_from_url(url):
